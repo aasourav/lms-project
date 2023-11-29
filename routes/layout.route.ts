3fceed6c7f9811009ac164b83ce52bd054c53d7d
Ahsan Amin <ahsan.amin@headless.ltd>
2023-11-29 23:21:49 +0600

    feat/52 edit faq hero categories

--- routes/layout.route.ts
+++ routes/layout.route.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { authRoles, isAuthenticated } from "../middleware/auth";
-import { createLayout } from "../controllers/layout.controller";
+import {
+  createLayout,
+  editLayout,
+  getLayoutByType,
+} from "../controllers/layout.controller";
 const layoutRouter = express.Router();
 
 layoutRouter.post(
@@ -9,4 +13,13 @@ layoutRouter.post(
   authRoles("admin"),
   createLayout
 );
+
+layoutRouter.put(
+  "/edit-layout",
+  isAuthenticated,
+  authRoles("admin"),
+  editLayout
+);
+
+layoutRouter.post("/get-layout", getLayoutByType);
 export default layoutRouter;
